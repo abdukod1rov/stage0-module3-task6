0898fa5a25661427744829c5b30e58f76af195a4
samandar7 <abdukodirovsamandar8@gmail.com>
2023-10-08 14:56:44 +0500

    idc

--- src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task6;
 
 public class IntegerTypeCasting {
     public static void main(String[] args) {
-    }
+        long name = 1000;
+        int second = 1000;
+        System.out.println(name);
+        System.out.println(second);
+        ;}
 }
